ce144db3e91151b86417bdbddf01492d254944c0
hbery-HYPERION <a.twardosz98@gmail.com>
2021-04-14 18:28:21 +0200

    lec.1 tasks

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from models import HelloResp
 
 app = FastAPI()
+app.counter = 0
 
 @app.get("/")
-def root():
+def root_view():
 	return {"message": "Hello World"}
+
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return app.counter
+
+@app.get("/hello/{name}", response_model=HelloResp)
+async def hello_name_view(name: str):
+    return HelloResp(msg=f"Hello {name}")
--- tests.py
+++ tests.py
@@ -1,11 +1,29 @@
 from fastapi.testclient import TestClient
+import pytest
+import json
 
 from main import app
 
 client = TestClient(app)
-
+client.counter = 0
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+@pytest.mark.parametrize("name", ['Adam', 'Miro', 'Jason'])
+def test_hello_name(name):
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.json() == {"msg": f"Hello {name}"}
+
+def test_counter():
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "1"
+    
+    # 2nd Try
+    response = client.get(f"/counter")
+    assert response.status_code == 200
+    assert response.text == "2"
