76878e7d0284678d84d0108e842f05ac143e4034
hbery-HYPERION <a.twardosz98@gmail.com>
2021-04-16 18:39:22 +0200

    task 1.4 h

--- util.py
+++ util.py
@@ -1,8 +1,13 @@
 import re
 
+search = re.compile(r'[^\W\d]', re.UNICODE)
+
 def calculate_names_length(*args, **kwargs):
     length = 0
     for name in args:
-        length += len(re.sub(r'[^a-zA-Z]', '', name))
+        length += len(search.findall(name))
 
     return length
+
+if __name__ == "__main__":
+    print(calculate_names_length('Świstak', "parówa123&*(*(&^% -"))
