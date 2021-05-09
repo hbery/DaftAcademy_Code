5f62a1a7f4373c1f4a8e2fe5745e42c71372da3f
hbery-HYPERION <a.twardosz98@gmail.com>
2021-05-09 20:28:14 +0200

    task 4.2 hw t4.1 still working

--- routers/database.py
+++ routers/database.py
@@ -1,4 +1,3 @@
-from os import curdir
 import sqlite3
 
 from fastapi import APIRouter, Request, Response, status, HTTPException
@@ -18,46 +17,70 @@ async def shutdown():
 @d_router.get('/categories', status_code=status.HTTP_200_OK)
 async def get_categories():
     cursor = d_router.dbconn.cursor()
+    cursor.row_factory = sqlite3.Row
     try:
         data = cursor.execute(
-            "SELECT CategoryID, CategoryName FROM categories ORDER BY CategoryID"
-            ).fetchall()
-        return JSONResponse(
-            {
-                "categories": [{"id": row[0], "name": row[1]} for row in data]
-            },
-            status_code=status.HTTP_200_OK            
-            )
+            """SELECT 
+                CategoryID as id, 
+                CategoryName as name 
+            FROM Categories 
+            ORDER BY id"""
+        ).fetchall()
+        
+        return dict(categories=data)
+    
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
             detail=f"{e}"
         )
     
-@d_router.get('/customers', status_code=status.HTTP_200_OK)
+@d_router.get('/customers',status_code=status.HTTP_200_OK)
 async def get_customers():
     cursor = d_router.dbconn.cursor()
+    cursor.row_factory = sqlite3.Row
     try:
         data = cursor.execute(
-            "SELECT CustomerID, CompanyName, (COALESCE(Address, '') || ' ' || COALESCE(PostalCode, '') || ' ' || COALESCE(City, '') || ' ' || COALESCE(Country, '')) FROM customers ORDER BY CustomerID"
-            ).fetchall()
-        return JSONResponse(
-            {
-                "customers": [{"id": row[0], "name": row[1], "full_address": row[2].strip()} for row in data]
-            },
-            status_code=status.HTTP_200_OK
-        )
+            """SELECT 
+                CustomerID as id, 
+                COALESCE(CompanyName, '') as name, 
+                (COALESCE(Address, '') || ' ' || COALESCE(PostalCode, '') || ' ' || COALESCE(City, '') || ' ' || COALESCE(Country, '')) as full_address 
+            FROM Customers 
+            ORDER BY id"""
+        ).fetchall()
+        
+        return dict(customers=data)
+    
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
             detail=f"{e}"
         )
         
-@d_router.get("/products/{int: id}")
-async def products():
-    cursor = d_router.db_connection.cursor()
-    products = cursor.execute("SELECT ProductName FROM Products").fetchall()
-    return {
-        "products": products,
-        "products_counter": len(products)
-    }
+@d_router.get("/products/{int: id}", status_code=status.HTTP_200_OK)
+async def products(id: int):
+    if not isinstance(id, int):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Record identified by given id: {id} does not exist."
+        )
+    
+    cursor = d_router.db_conn.cursor()
+    cursor.row_factory = sqlite3.Row
+    
+    products = cursor.execute("""
+                              SELECT 
+                                ProductID as id, 
+                                ProductName as name 
+                              FROM Products 
+                              WHERE id = :pid""", 
+                              {"pid": id}
+    ).fetchone()
+    
+    if products:
+        return dict(id=products["id"], name=products["name"])
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Record identified by given id: {id} does not exist."
+        )
